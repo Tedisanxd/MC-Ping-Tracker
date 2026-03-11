826ca4aabb1e60584cb70164d76bc5fdf09ca8b1
Tedisanxd <braydenhill799@gmail.com>
2026-03-11 16:32:14 -0700

    Delete PingTracker V1.1.5 directory

--- PingTracker V1.1.5/install.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-title MC Ping Tracker - Install
-echo Installing dependencies...
-pip install -r requirements.txt
-echo.
-echo Done! Run "run.bat" to start.
-pause
--- PingTracker V1.1.5/run.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-title MC Ping Tracker
-pythonw main.py
-if errorlevel 1 (
-    python main.py
-    pause
-)
